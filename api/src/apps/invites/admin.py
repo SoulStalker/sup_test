cd2089a1bf7b9b0baaa8127e2230974123744e2c
Almaz Galimov <almazwork@gmail.com>
2024-11-02 12:52:37 +0500

    добавил приложение инвайтов. создал модель

---
+++ api/src/apps/invites/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from src.models.invites import Invite
+
+
+@admin.register(Invite)
+class InviteAdmin(admin.ModelAdmin):
+    list_display = ("link", "status", "created_at", "expires_at")
+    list_filter = ("status", "created_at", "expires_at")
+    search_fields = ("link", "status", "created_at", "expires_at")
