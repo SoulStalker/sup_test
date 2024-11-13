c100ad76f3f91cc596803fb3223de1534182c6e8
Almaz Galimov <almazwork@gmail.com>
2024-11-13 16:05:09 +0500

    разделил валидаторы

--- api/src/models/models.py
+++ api/src/models/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from src.models.managers import CustomUserManager
-from ..domain.validators.validators import ModelValidator
+
+from .validators import ModelValidator
 
 
 class Role(models.Model):
