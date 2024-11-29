1ef607a2971fe60d2ca4bf69242fe763f35e01f1
Almaz Galimov <almazwork@gmail.com>
2024-11-29 18:35:53 +0500

    убрал дубли js

--- api/src/static/script/invite.js
+++ api/src/static/script/invite.js
@@ -138,19 +138,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-
-// Пагинация
-document.addEventListener('DOMContentLoaded', function () {
-    const rowsPerPageSelect = document.getElementById('rows-per-page');
-    const rowsPerPageForm = document.getElementById('rows-per-page-form');
-
-    rowsPerPageSelect.addEventListener('change', function () {
-        // Сбрасываем номер страницы на 1 при изменении количества элементов
-        const pageInput = rowsPerPageForm.querySelector('input[name="page"]');
-        if (pageInput) {
-            pageInput.value = '1';
-        }
-        rowsPerPageForm.submit();
-    });
-});
