c2059e884413dd97e022232fd7aa1eba8735f6f7
Almaz Galimov <almazwork@gmail.com>
2024-12-02 16:38:00 +0500

    добавил говнокода

--- api/src/apps/teams/forms.py
+++ api/src/apps/teams/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.utils.functional import cached_property
 
 User = get_user_model()
 
@@ -7,16 +8,26 @@ User = get_user_model()
 class CreateTeamForm(forms.Form):
     id = forms.IntegerField()
     name = forms.CharField()
-    members_count = forms.IntegerField()
+    participants = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        label="Участники",
+        widget=forms.CheckboxSelectMultiple,
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["participants"] = forms.MultipleChoiceField(
-            choices=[(user.id, user.name) for user in User.objects.all()],
-            widget=forms.CheckboxSelectMultiple,
-            required=False,
+        self.fields["participants"] = forms.ModelMultipleChoiceField(
+            queryset=self.users_queryset, widget=forms.CheckboxSelectMultiple
         )
 
+    @cached_property
+    def users_queryset(self):
+        """Возвращает активных пользователей для выбора в форме."""
+        return User.objects.filter(is_active=True)
+
     def clean(self):
+        """Очистка данных формы."""
         cleaned_data = super().clean()
+        if cleaned_data.get("participants") is None:
+            cleaned_data["participants"] = []
         return cleaned_data
