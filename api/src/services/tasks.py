5e8b82869ed04da704e1d064c7e19b8d30c96f51
Almaz Galimov <almazwork@gmail.com>
2024-12-14 21:49:49 +0500

    добавил celery и redis

---
+++ api/src/services/tasks.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from src.apps.users.repository import UserRepository
+
+from .celery import app
+
+user_repository = UserRepository()
+
+
+@app.task
+def send_email_to_user(**kwargs):
+    subject = kwargs.get("subject", "Добро пожаловать!")
+    message = kwargs.get(
+        "message",
+        f"Здравствуйте, {kwargs.get("name")}!\n\nВаш аккаунт успешно создан.",
+    )
+    from_email = settings.DEFAULT_FROM_EMAIL
+    recipient_list = [kwargs.get("email")]
+    send_mail(
+        subject, message, from_email, recipient_list, fail_silently=False
+    )
