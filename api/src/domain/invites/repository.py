52b5461be16d2ed94311eaea858eb8ba58dbd817
Almaz Galimov <almazwork@gmail.com>
2024-12-23 15:37:03 +0500

    Отрефакторил, уменьшил количество кода. добавив родительские классы

--- api/src/domain/invites/repository.py
+++ api/src/domain/invites/repository.py
@@ -1,30 +1,18 @@
-import abc
+from abc import abstractmethod
 
+from src.domain.base import BaseRepository
 from src.domain.invites.dtos import InviteDTO
 
 
-class IInviteRepository(abc.ABC):
-    @abc.abstractmethod
-    def get_invite_by_id(self, invite_id: int):
+class IInviteRepository(BaseRepository):
+    @abstractmethod
+    def create(self) -> InviteDTO:
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def get_invites_list(self) -> list[InviteDTO]:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def create(self, email) -> InviteDTO:
-        print('create repos')
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def delete(self, invite_id: int):
-        raise NotImplementedError
-
-    @abc.abstractmethod
+    @abstractmethod
     def update_status(self, invite_id: int, status: str):
         raise NotImplementedError
-    
-    @abc.abstractmethod
+
+    @abstractmethod
     def create_inviteDTO(self, invitation_code: str):
         raise NotImplementedError
