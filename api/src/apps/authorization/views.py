8c5a7d2490310538b77abe0a286f5c9d07c35dd4
MAX\demen <demenik12345@yandex.ru>
2024-12-26 12:07:16 +0400

    правки авторизации

--- api/src/apps/authorization/views.py
+++ api/src/apps/authorization/views.py
@@ -1,26 +1,27 @@
 from django.contrib.auth import authenticate, login, REDIRECT_FIELD_NAME
+from django.contrib.auth import logout
 from django.db.utils import IntegrityError
-from django.http import JsonResponse
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from src.apps.authorization.forms import AuthorizationForm
 from src.apps.custom_view import BaseView
 
 
-class Userauthorization(BaseView):
+class UserAuthorization(BaseView):
     """Авторизация пользователя"""
 
     login_required = False
 
     def get(self, request):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('authorization:logout'))
         form = AuthorizationForm()
-        return render(
-            request,
-            "auth.html",
-            {"form": form},
-        )
+        return render(request, "auth.html", {"form": form})
 
     def post(self, request):
         form = AuthorizationForm(request.POST)
+        next_url = request.GET.get(REDIRECT_FIELD_NAME, '/default-url/')
         if form.is_valid():
             cd = form.cleaned_data
             try:
@@ -29,8 +30,10 @@ class Userauthorization(BaseView):
                 )
                 if user and user.is_active:
                     login(request, user)
-                    next_url = request.GET.get(REDIRECT_FIELD_NAME, '/default-url/')
-                    return redirect(next_url)
+                    if next_url == '/default-url/':
+                        return redirect('/meets/')
+                    else:
+                        return redirect(next_url)
                 else:
                     return JsonResponse(
                         {
@@ -47,3 +50,13 @@ class Userauthorization(BaseView):
                     },
                     status=404,
                 )
+
+
+class UserLogout(BaseView):
+
+    def get(self, request):
+        return render(request, "logout.html")
+    
+    def post(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse('authorization:logout'))
