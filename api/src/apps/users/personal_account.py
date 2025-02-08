a7ee92537e7da1ebc575d96c9eec5d30656eac4b
MAX\demen <demenik12345@yandex.ru>
2025-02-08 17:45:27 +0400

    добавил инфы в лк

--- api/src/apps/users/personal_account.py
+++ api/src/apps/users/personal_account.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 from src.apps.custom_view import BaseView
 
 
@@ -10,14 +11,23 @@ class PersonalAccount(BaseView):
 
     def get(self, request, *args, **kwargs):
         user = request.user
-        #список за что ответсвенный
+        #список задач за которые ответсвенный
         task_responsible = self.task_service.get_list_responsible(user_id=user.id)
-        #список у каких задач автор
-        task_contributor = self.task_service.get_list_contributor(user_id=user.id)
+        #список задач у который автор
+        task_contributor = self.task_service.get_list_contributor(user_id=user.id)\
+        #количество завершённых задач
+        task_success = len(tuple(filter(lambda x: x.status == 'Готов', task_contributor)))
+        #список проектов в которых участник
+        projects_participants = self.project_service.get_list_participants(user_id=user.id)
+        personal_account_url = reverse("users:personal_account")
+        
         context = {
                 'user': user,
                 'task_responsible': task_responsible,
+                'task_success': task_success,
                 'task_contributor': task_contributor,
+                'projects_participants': projects_participants,
+                'personal_account_url': personal_account_url,
             }
   
         return render(self.request, "personal_account.html", context)
