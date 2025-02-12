1f245dc7ac9c322ed88ac03ddb41e38a2481b21c
Almaz Galimov <almazwork@gmail.com>
2025-02-12 11:49:29 +0500

    Добавил комментарии и типизацию в инвайтах

--- api/src/domain/invites/dtos.py
+++ api/src/domain/invites/dtos.py
@@ -4,6 +4,16 @@ from datetime import datetime
 
 @dataclass
 class InviteDTO:
+    """
+    DTO (Data Transfer Object) для передачи данных о приглашении.
+
+    :param id: Уникальный идентификатор приглашения.
+    :param link: Ссылка на приглашение.
+    :param status: Статус приглашения.
+    :param created_at: Дата и время создания приглашения.
+    :param expires_at: Дата и время истечения срока действия приглашения.
+    """
+
     id: int
     link: str
     status: str
