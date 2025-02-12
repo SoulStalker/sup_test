d7ccd777d35f69c090e6d250a73a3d5da2ddf29a
Almaz Galimov <almazwork@gmail.com>
2025-02-12 16:09:37 +0500

    Добавил комментарии и типизацию в командах

--- api/src/domain/teams/repository.py
+++ api/src/domain/teams/repository.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from typing import Any
+from typing import Optional, Tuple
 
 from src.domain.base import BaseRepository
 
@@ -7,13 +7,31 @@ from .dtos import CreateTeamDTO, TeamDTO
 
 
 class ITeamRepository(BaseRepository):
+    """
+    Интерфейс репозитория для работы с командами.
+    """
 
     @abstractmethod
-    def create(self, dto: CreateTeamDTO) -> tuple[None, Any] | TeamDTO:
+    def create(
+        self, dto: CreateTeamDTO
+    ) -> Tuple[Optional[TeamDTO], Optional[str]]:
+        """
+        Создает новую команду.
+
+        :param dto: DTO команды.
+        :return: Кортеж (DTO команды, ошибка), где DTO — созданная команда, а ошибка — сообщение об ошибке.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def update(
         self, pk: int, dto: TeamDTO
-    ) -> [tuple[None, Any] | TeamDTO, Any]:
+    ) -> Tuple[Optional[TeamDTO], Optional[str]]:
+        """
+        Обновляет данные команды.
+
+        :param pk: Идентификатор команды.
+        :param dto: DTO команды с обновленными данными.
+        :return: Кортеж (DTO команды, ошибка), где DTO — обновленная команда, а ошибка — сообщение об ошибке.
+        """
         raise NotImplementedError
